36248f339d77ebac4c64bb191449d22ba35b2630
ege pekgüleç <egepekgulec_@outlook.com>
2024-04-27 14:56:26 +0300

    added variables and numberOfLines method

--- System.java
+++ System.java
@@ -1,9 +1,35 @@
 package BankSystem;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class System{
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
         String currentDir = "/home/egecik/Masaüstü/JavaDersleri/BankSystem";// example/..../example/BankSystem/
         String accountInfoPath = currentDir + "AccountInfo.txt";
         String transferInfoPath = currentDir + "TransferInfo.txt";
+
+        int numberOfAccounts = numberOflines(accountInfoPath); 
+        int[] acctNums = new int[numberOfAccounts];
+        String[] names = new String[numberOfAccounts];
+        String[] surnames = new String[numberOfAccounts];
+        double[] balances = new double[numberOfAccounts];
+
+        int numberOfTransfers = numberOflines(transferInfoPath);
+        int[] transferIdList = new int[numberOfTransfers];
+        int[] acctNumFromList = new int[numberOfTransfers];
+        int[] acctNumToList = new int[numberOfTransfers];
+        double[] amountlist = new double[numberOfTransfers];
+    }
+    static int numberOflines(String filePath) throws FileNotFoundException{
+        Scanner scan = new Scanner(new File(filePath));
+        int count = 0;
+        while(scan.hasNextLine()){
+            scan.nextLine();
+            count ++;
+        }
+        scan.close();
+        return count;
     }
 }
