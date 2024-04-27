2f8a431dcffc28a947f1fc4a80e63b3e11b28966
ege pekgüleç <egepekgulec_@outlook.com>
2024-04-27 14:52:20 +0300

    File paths added

--- System.java
+++ System.java
@@ -2,6 +2,8 @@ package BankSystem;
 
 public class System{
     public static void main(String[] args){
-        
+        String currentDir = "/home/egecik/Masaüstü/JavaDersleri/BankSystem";// example/..../example/BankSystem/
+        String accountInfoPath = currentDir + "AccountInfo.txt";
+        String transferInfoPath = currentDir + "TransferInfo.txt";
     }
 }
